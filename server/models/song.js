7af4e794efd2e99f09f140738a32bce4bab58b70
Ray Chung <ray.j.chung@gmail.com>
2021-01-31 23:48:49 -0800

    Fix song mongoose schema to enable usePushEach.

--- server/models/song.js
+++ server/models/song.js
@@ -11,6 +11,8 @@ const SongSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'lyric'
   }]
+}, {
+  usePushEach: true
 });
 
 SongSchema.statics.addLyric = function(id, content) {
@@ -18,17 +20,17 @@ SongSchema.statics.addLyric = function(id, content) {
 
   return this.findById(id)
     .then(song => {
-      const lyric = new Lyric({ content, song })
-      song.lyrics.push(lyric)
+      const lyric = new Lyric({ content, song });
+      song.lyrics.push(lyric);
       return Promise.all([lyric.save(), song.save()])
         .then(([lyric, song]) => song);
     });
-}
+};
 
 SongSchema.statics.findLyrics = function(id) {
   return this.findById(id)
     .populate('lyrics')
     .then(song => song.lyrics);
-}
+};
 
 mongoose.model('song', SongSchema);
